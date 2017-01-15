0a698d54c46fc136a4e1693b8a0504368f71e33b
Ryan Tuller <ryantulleris@gmail.com>
2017-01-15 00:22:35 -0700

    stylistic changes on the theme. changed content main to span the full width of the screen

--- app/src/main/java/com/example/bigmemes/creatinecamera/SideBySideFragment.java
+++ app/src/main/java/com/example/bigmemes/creatinecamera/SideBySideFragment.java
@@ -3,9 +3,12 @@ package com.example.bigmemes.creatinecamera;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by Ryan Mitchell Tuller on 12/13/2016.
@@ -15,12 +18,17 @@ public class SideBySideFragment extends Fragment{
 
     View myView;
 
-    @Nullable
-    @Override
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView  = inflater.inflate(R.layout.side_by_side_layout,container,false);
+
+        ImageView imageViewTop = (ImageView) myView.findViewById(R.id.topPicture);
+        ImageView imageViewBottom = (ImageView) myView.findViewById(R.id.bottomPicture);
+
+
         return myView;
     }
+
 
 
 }
