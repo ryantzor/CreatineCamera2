0e1e820b925c1557fd428595a1c6b156a64f3dc8
Ryan Tuller <ryantulleris@gmail.com>
2017-05-25 13:12:48 -0600

    added layout changes and enhancements

--- app/src/main/java/com/example/bigmemes/creatinecamera/SideBySideFragment.java
+++ app/src/main/java/com/example/bigmemes/creatinecamera/SideBySideFragment.java
@@ -1,5 +1,6 @@
 package com.example.bigmemes.creatinecamera;
 
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -8,7 +9,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.support.design.widget.Snackbar;
 import android.widget.ImageView;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 
 /**
  * Created by Ryan Mitchell Tuller on 12/13/2016.
@@ -22,13 +26,28 @@ public class SideBySideFragment extends Fragment{
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         myView  = inflater.inflate(R.layout.side_by_side_layout,container,false);
 
-        ImageView imageViewTop = (ImageView) myView.findViewById(R.id.topPicture);
-        ImageView imageViewBottom = (ImageView) myView.findViewById(R.id.bottomPicture);
 
 
+        //add the image view objects
+        ImageView topPicture = (ImageView) myView.findViewById(R.id.topPicture);
+        ImageView bottomPicture = (ImageView) myView.findViewById(R.id.bottomPicture);
+
+/*        topPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog alertDialog = new AlertDialog.Builder(SideBySideFragment.this).create();
+                alertDialog.setTitle("Alert");
+                alertDialog.setMessage("Alert message to be shown");
+                alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        });
+                alertDialog.show();
+            }
+        });*/
         return myView;
     }
-
-
 
 }
